e37b0b65ce3daa0076fbdfd7df1a70ad8597f93f
arang <dkfkddl95@naver.com>
2023-08-07 00:01:51 +0900

    feat/강좌제안전체목록API_#54

--- src/main/java/com/seniorjob/seniorjobserver/repository/LectureProposalRepository.java
+++ src/main/java/com/seniorjob/seniorjobserver/repository/LectureProposalRepository.java
@@ -1,8 +1,11 @@
 package com.seniorjob.seniorjobserver.repository;
 
 import com.seniorjob.seniorjobserver.domain.entity.LectureProposalEntity;
+import com.seniorjob.seniorjobserver.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface LectureProposalRepository extends JpaRepository<LectureProposalEntity, Long>{
